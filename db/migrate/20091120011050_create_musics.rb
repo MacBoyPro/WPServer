3218acbe03a88d61ddbf7899272ac106a924777c
Lee Lundrigan <leelundrigan@gmail.com>
2009-11-19 20:11:47 -0500

    added database models for music and videos

---
+++ db/migrate/20091120011050_create_musics.rb
@@ -0,0 +1,15 @@
+class CreateMusics < ActiveRecord::Migration
+  def self.up
+    create_table :musics do |t|
+      t.string :title
+      t.string :description
+      t.string :url
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :musics
+  end
+end
